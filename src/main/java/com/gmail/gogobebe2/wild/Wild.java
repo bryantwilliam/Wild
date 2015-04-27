aea266e8ce21a4ed2d0dcd97dc8d477f4d658695
gogobebe2 <gogobebe2@gmail.com>
2015-04-27 19:54:48 +1200

    1.0.1-DEVELOPMENT-SNAPSHOT

--- src/main/java/com/gmail/gogobebe2/wild/Wild.java
+++ src/main/java/com/gmail/gogobebe2/wild/Wild.java
@@ -11,6 +11,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Wild extends JavaPlugin {
     private List<Player> playersOnCooldown = new ArrayList<>();
@@ -26,18 +27,49 @@ public class Wild extends JavaPlugin {
             if (args.length > 0) {
                 player.sendMessage(ChatColor.GREEN + "You entered some arguments to the command /wild. I will just ignore them :P");
             }
-            //
 
+            teleRandomLocation(player, -5000, 5000);
+            return true;
         }
         return false;
     }
 
+    public static double randDouble(double min, double max) {
+        Random r = new Random();
+        double randomValue = min + (max - min) * r.nextDouble();
+        return randomValue;
+    }
+
+    private void teleRandomLocation(Player player, double min, double max) {
+        Location location = player.getLocation();
+        while (true) {
+            location.setX(randDouble(min, max));
+            location.setZ(randDouble(min, max));
+            Location ground = getGround(location);
+            if (ground != null) {
+                location = ground;
+                break;
+            }
+        }
+        player.teleport(location);
+        player.sendMessage(ChatColor.AQUA + "" + ChatColor.BOLD + "After exploring the land, you've finally come across"
+                + "some land that looks like a good spot to build your faction home...");
+        player.sendMessage(ChatColor.AQUA + "" + ChatColor.ITALIC + "Goodluck on your adventures and missions!!!");
+    }
+
     private Location getGround(Location location) {
+        return getGround(location, 260D);
+    }
+
+    private Location getGround(Location location, double y) {
+        location.setY(y);
         Block block = location.getBlock();
-        double y = location.getY();
-        if (block.getType().equals(Material.AIR)) {
+        if (y < 0) {
+            return null;
+        }
+        else if (block.getType().equals(Material.AIR) || block.getType().equals(Material.LAVA) || block.getType().equals(Material.WATER)) {
             location.setY(y - 1);
-            return getGround(location);
+            return getGround(location, y);
         }
         return location;
     }
